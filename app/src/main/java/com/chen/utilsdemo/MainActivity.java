a2f98609f35691a85ef07a8803b68b97e794dd2a
chenxiaozong <chenxzong@qq.com>
2020-04-02 22:00:37 +0800

    update

--- app/src/main/java/com/chen/utilsdemo/MainActivity.java
+++ app/src/main/java/com/chen/utilsdemo/MainActivity.java
@@ -12,13 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        ChenLog.i("日志打印，","tag1","info2");
-=======
-<<<<<<< HEAD
+
         ChenLog.i("test","oncreate");
-=======
->>>>>>> 2d7d7afe277f7e9c6059410a96d0fcc05d8716c4
->>>>>>> 1aae552ba13112ac9630ac8b7c9c7ae9056d24d6
+
     }
 }
